0783b7a193d1974bbe50d2ada44be7542613fe82
Ohinoyi Moiza <163584196+Ohimoiza1205@users.noreply.github.com>
2024-04-03 00:57:02 -0500

    Create 0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Create a class BasicCache that inherits from
+BaseCaching and is a caching system:
+
+def get(self, key):
+Must return the value in self.cache_data linked
+to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """_summary_
+    """
+
+    def __init__(self):
+        """_summary_
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """_summary_
+
+        Args:
+                key (_type_): _description_
+                item (_type_): _description_
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key
+
+        Args:
+                key (_type_): _description_
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
